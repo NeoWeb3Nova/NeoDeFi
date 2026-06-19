e792fa32e93059121bfbb04341121609934637e4
NeoWeb3Nova <221855057+NeoWeb3Nova@users.noreply.github.com>
2026-06-19 12:16:01 +0800

    feat: Add deployment script and transaction details for ETFQuoter contract
    - Created a new deployment script for the ETFQuoter contract in `script/DeployETFQuoter.s.sol`.
    - Implemented the deployment logic, including the use of a private key for broadcasting the transaction.
    - Added a JSON file `run-latest.json` to store transaction details, including contract address and transaction hash.

---
+++ script/DeployETFQuoter.s.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.27;
+
+import {Script, console} from "forge-std/Script.sol";
+import {ETFQuoter} from "../src/ETFQuoter.sol";
+
+// Deploy script:
+// forge script script/DeployETFQuoter.s.sol --rpc-url $SEPOLIA_RPC_URL --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY -vv
+
+contract DeployETFQuoter is Script {
+    address public constant UNISWAP_V3_QUOTER =
+        0x43C4147CbaF8eeA99A79F3040E01CC5e6830Cc19;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        console.log("Deployer address:", deployerAddress);
+
+        vm.startBroadcast(deployerPrivateKey);
+        // Deploy the ETFQuoter contract
+        address etfQuoter = address(
+            new ETFQuoter(UNISWAP_V3_QUOTER)
+        );
+        console.log("ETFQuoter deployed at:", etfQuoter);
+        vm.stopBroadcast();
+    }
+}
